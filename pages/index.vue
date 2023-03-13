3365f839b3a6a1c6bb6c22cd547026dbc9c2540d
simon_read <s.read@walz.de>
2023-03-13 11:42:51 +0100

    Anmelde Seite angelegt

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <div
-    class="bg-cover bg-[url('/public/tmlunch.png')] w-full h-full flex justify-center"
-  >
-    <div class="rounded p-10 bg-lime-300 text-center opacity-20 flex items-end">
-      <h1 class="font-bold text-2xl">Team Events</h1>
+  <div class="">
+    <div class="m-5 border rounded w-1/2">
+      <div class="m-5">
+        <h1 class="font-bold text-lg">Wilkommen bei der Team Event App</h1>
+      </div>
+      <div class="m-5">
+        <input class="p-1 rounded" placeholder="Name" />
+      </div>
+      <div class="m-5">
+        <button
+          class="text-blue-700 font-semibold py-2 px-4 border border-blue-500 hover:border-transparent hover:text-white hover:bg-blue-500 rounded"
+        >
+          Anmelden
+        </button>
+      </div>
     </div>
   </div>
 </template>
